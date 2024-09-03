fe016103c6029fc98436c750de660dbaa18b3711
vadshustow <vadshustow@yandex.ru>
2024-09-03 23:34:59 +0700

    Переименовал метод newLeader() в addRacer()

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -3,7 +3,7 @@ public class Race {
     String leader = "";
     int distance = 0;
 
-    public void newLeader(String name, int speed) {
+    public void addRacer(String name, int speed) {
         int newDistance = speed * 24;
         if (distance < newDistance) {
             distance = newDistance;
